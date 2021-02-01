c636215205cc87ab5e1d602c78fa22e7f113dc9f
Rene Saarsoo <nene@triin.net>
2021-02-01 20:55:08 +0200

    Link to actual LICENSE file

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,8 +5,11 @@ export default function Footer() {
   return (
     <div className="footer">
       <ul>
-        <li>Zwift Workout v1.7 &copy; 2020 Sharpify Ltd All Rights Reserved</li>
-        <li><a href="https://github.com/breiko83/zwo-editor/issues" target="blank">Report an issue</a></li>        
+        <li>
+          Zwift Workout v1.7
+          (<a href="https://github.com/breiko83/zwo-editor/blob/master/LICENSE.md" target="blank">open source / MIT license</a>)
+        </li>
+        <li><a href="https://github.com/breiko83/zwo-editor/issues" target="blank">Report an issue</a></li>
       </ul>
     </div>
   )
