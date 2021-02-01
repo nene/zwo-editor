9fedd95d59214baab8adf2ac7e5eceb6f04b9946
Rene Saarsoo <nene@triin.net>
2021-02-01 20:51:51 +0200

    Remove Privacy Policy - not relevant

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,24 +1,13 @@
-import React, {useState} from 'react'
+import React from 'react'
 import './Footer.css'
-import Privacy from '../Privacy/Privacy'
-import Popup from '../Popup/Popup'
 
 export default function Footer() {
-  const [showPrivacy, setShowPrivacy] = useState(false)
-
   return (
     <div className="footer">
       <ul>
         <li>Zwift Workout v1.7 &copy; 2020 Sharpify Ltd All Rights Reserved</li>
-        <li><a href="#privacy" onClick={() => setShowPrivacy(true)}>Privacy Policy</a></li>
         <li><a href="https://github.com/breiko83/zwo-editor/issues" target="blank">Report an issue</a></li>        
       </ul>
-       
-      {showPrivacy &&
-        <Popup width="50%" height="50%" dismiss={() => setShowPrivacy(false)}>
-          <Privacy />
-        </Popup>
-      }
     </div>
   )
 }
