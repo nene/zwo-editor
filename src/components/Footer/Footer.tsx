c3ecab138bf0d1ac812a262dd71ae886f9fce591
Rene Saarsoo <nene@triin.net>
2021-02-01 20:47:26 +0200

    Remove Terms Of Service
    As I was told by Carlo Schiesaro, these terms aren't correct anyway,
    the LICENSE is what matters.
    
    https://github.com/breiko83/zwo-editor/issues/26

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,28 +1,19 @@
 import React, {useState} from 'react'
 import './Footer.css'
 import Privacy from '../Privacy/Privacy'
-import Terms from '../Terms/Terms'
 import Popup from '../Popup/Popup'
 
 export default function Footer() {
-
-  const [showTerms, setShowTerm] = useState(false)
   const [showPrivacy, setShowPrivacy] = useState(false)
 
   return (
     <div className="footer">
       <ul>
         <li>Zwift Workout v1.7 &copy; 2020 Sharpify Ltd All Rights Reserved</li>
-        <li><a href="#terms" onClick={() => setShowTerm(true)}>Terms of Service</a></li>
         <li><a href="#privacy" onClick={() => setShowPrivacy(true)}>Privacy Policy</a></li>
         <li><a href="https://github.com/breiko83/zwo-editor/issues" target="blank">Report an issue</a></li>        
       </ul>
        
-      {showTerms &&
-        <Popup width="90%" height="80%" dismiss={() => setShowTerm(false)}>      
-          <Terms />
-        </Popup>
-      }
       {showPrivacy &&
         <Popup width="50%" height="50%" dismiss={() => setShowPrivacy(false)}>
           <Privacy />
