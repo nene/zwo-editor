afb9ed5b174ec134535f7e3a0aaa6b9198c61a5d
Rene Saarsoo <nene@triin.net>
2021-02-03 14:21:01 +0200

    Add test for showing instruction editor

--- src/components/InstructionEditor/__tests__/InstructionEditor.test.tsx
+++ src/components/InstructionEditor/__tests__/InstructionEditor.test.tsx
@@ -4,24 +4,39 @@ import renderer from 'react-test-renderer';
 import { createInstruction } from '../../../types/Instruction';
 import createMode from '../../../modes/createMode';
 import { Duration } from '../../../types/Length';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-test('InstructionEditor renders correctly', () => {
-  const mode = createMode({sportType: "bike", ftp: 200, weight: 75, runningTimes: [], lengthType: "time"});
-  const instruction = createInstruction({
-    text: 'This is a comment',
-    offset: new Duration(300),
-  }, mode);
+describe('InstructionEditor', () => {
+  let component: renderer.ReactTestRenderer;
 
-  const component = renderer.create(
-    <InstructionEditor
-      instruction={instruction}
-      width={500}
-      index={0}
-      onChange={() => { }}
-      onDelete={() => { }}
-      mode={mode}
-    />,
-  );
+  beforeEach(() => {
+    const mode = createMode({sportType: "bike", ftp: 200, weight: 75, runningTimes: [], lengthType: "time"});
+    const instruction = createInstruction({
+      text: 'This is a comment',
+      offset: new Duration(300),
+    }, mode);
+  
+    component = renderer.create(
+      <InstructionEditor
+        instruction={instruction}
+        width={500}
+        index={0}
+        onChange={() => { }}
+        onDelete={() => { }}
+        mode={mode}
+      />,
+    );
+  })
 
-  expect(component).toMatchSnapshot();
+  it('renders vertical line and icon', () => {
+    expect(component).toMatchSnapshot();
+  });
+
+  it('shows the editor when icon clicked', () => {
+    renderer.act(() => {
+      component.root.findByType(FontAwesomeIcon).props.onMouseDown();
+    })
+  
+    expect(component).toMatchSnapshot();
+  });
 });
