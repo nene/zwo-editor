ac3e1e2e775504c0be74482b16d0419f84a9348b
Rene Saarsoo <nene@triin.net>
2021-02-07 00:11:35 +0200

    Use styled-components in Title

--- src/components/EditorPage/Title.tsx
+++ src/components/EditorPage/Title.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 // Displays workut name, description & author
 
@@ -11,12 +12,27 @@ interface TitleProps {
 
 const Title: React.FC<TitleProps> = ({ name, author, description, onClick }) => {
   return (
-    <div className="title" onClick={onClick}>
-      <h1>{name}</h1>
-      <div className="description">{description}</div>
-      <p>{author ? `by ${author}` : ''}</p>
-    </div>
+    <Container onClick={onClick}>
+      <Heading>{name}</Heading>
+      <div>{description}</div>
+      <Author>{author ? `by ${author}` : ''}</Author>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  flex: 1;
+  cursor: pointer;
+`;
+
+const Heading = styled.h1`
+  padding: 10px 0;
+  margin: 0;
+`;
+
+const Author = styled.p`
+  font-style: italic;
+  margin: 10px 0 0 0;
+`;
 
 export default Title;
