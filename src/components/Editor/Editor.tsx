bb554d7810fa300363ace0da7877dbb8a002f4ae
Rene Saarsoo <nene@triin.net>
2021-02-06 23:49:14 +0200

    Use styled-components in Editor

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -16,6 +16,7 @@ import { selectMode } from '../../rdx/state/mode';
 import { ConnectedProps } from '../../types/ConnectedProps';
 import { selectSelectedId, clearSelection, setSelectedId } from '../../rdx/state/selectedId';
 import SelectionToolbar from '../SelectionToolbar/SelectionToolbar';
+import styled from 'styled-components';
 
 const mapStateToProps = (state: RootState) => ({
   lengthType: selectLengthType(state),
@@ -89,25 +90,82 @@ const Editor = ({
   )
 
   return (
-    <div id="editor" className='editor'>
+    <Container>
       {selectedId && <SelectionToolbar />}
-      <div className='canvas' ref={canvasRef}>          
-        {selectedId &&
-          <div className='fader' style={{width: canvasRef.current?.scrollWidth}} onClick={() => clearSelection()}></div>
-        }
-        <div className='segments' ref={segmentsRef}>
-          {intervals.map(renderInterval)}
-        </div>
 
-        <div className='slider'>
+      <Canvas ref={canvasRef}>
+        {selectedId &&
+          <Fader style={{width: canvasRef.current?.scrollWidth}} onClick={clearSelection} />
+        }
+        <Segments ref={segmentsRef}>
+          {intervals.map(renderInterval)}
+        </Segments>
+
+        <Slider>
           {instructions.map((instruction, index) => renderInstruction(instruction, index))}
-        </div>
+        </Slider>
 
         {lengthType === "time" ? <TimeAxis width={xAxisWidth} /> : <DistanceAxis width={xAxisWidth} />}
-      </div>
+      </Canvas>
       <ZoneAxis />
-    </div>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  flex: 1;
+  position: relative;  
+  white-space: nowrap;
+  background-color: white;
+
+  padding: 0px 40px;  
+  margin: 10px auto;
+  overflow-x: hidden;
+  width: 100%;
+  max-width: 1280px;
+  -webkit-box-shadow: 0px 0px 27px -9px rgba(0,0,0,0.35);
+  -moz-box-shadow: 0px 0px 27px -9px rgba(0,0,0,0.35);
+  box-shadow: 0px 0px 27px -9px rgba(0,0,0,0.35);
+  border-radius: 5px;
+`;
+
+const Canvas = styled.div`
+  position: absolute;
+  width: 100%;
+  max-width: 1320px;
+  height: 100%;
+  overflow-y: hidden;
+  overflow-x: visible;
+  bottom: 10px;
+`;
+
+const Segments = styled.div`
+  position: absolute;
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: flex-start;
+  align-items: flex-end;  
+  bottom: 0px;
+  padding-bottom: 40px;
+  margin-right: 100px;
+`;
+
+const Fader = styled.div`
+  position: absolute;
+  background-color: rgba(255, 255, 255, 0.9);
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 50000px;
+  height: 100%;
+  z-index: 1;
+`;
+
+const Slider = styled.div`
+  position: absolute;  
+  top: 10px;
+  left: 0px;
+`;
 
 export default connect(mapStateToProps, mapDispatchToProps)(Editor)
