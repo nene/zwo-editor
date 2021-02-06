ca2604fd1cd7edd1c75dae20b7e3071a58d84bf7
Rene Saarsoo <nene@triin.net>
2021-02-06 21:55:42 +0200

    Use styled-components for PaceSelector

--- src/components/SelectionToolbar/PaceSelector.tsx
+++ src/components/SelectionToolbar/PaceSelector.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import { PaceType } from "../../types/PaceType";
 
 interface PaceSelectorProps {
@@ -7,13 +8,21 @@ interface PaceSelectorProps {
 }
 
 const PaceSelector = ({ value, onChange }: PaceSelectorProps) => (
-  <select name="pace" value={value} onChange={(e) => onChange(parseInt(e.target.value))} className="selectInput">
+  <StyledSelect name="pace" value={value} onChange={(e) => onChange(parseInt(e.target.value))}>
     <option value="0">1 Mile Pace</option>
     <option value="1">5K Pace</option>
     <option value="2">10K Pace</option>
     <option value="3">Half Marathon Pace</option>
     <option value="4">Marathon Pace</option>
-  </select>
+  </StyledSelect>
 );
+
+const StyledSelect = styled.select`
+  font-size: 16px;
+  background-color: rgba(0, 0, 0, 0.7);
+  color: white;
+  padding: 10px;
+  border-radius: 5px;
+`;
 
 export default PaceSelector;
