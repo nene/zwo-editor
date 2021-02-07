89c973287473f6863137f0a4f36f80cc6619d273
Rene Saarsoo <nene@triin.net>
2021-02-07 20:10:24 +0200

    Use styled-components in ZoneAxis

--- src/components/Axis/ZoneAxis.tsx
+++ src/components/Axis/ZoneAxis.tsx
@@ -1,17 +1,36 @@
 import React from "react";
+import styled from "styled-components";
 import { Zones } from "../../types/Zones";
-import "./ZoneAxis.css";
 
 const ZoneAxis = () => (
-  <div className="zone-axis">
+  <Container>
     {Object.entries(Zones)
       .reverse()
       .map(([name, zone]) => (
-        <div key={name} style={{ height: 250 * zone.max }}>
+        <ZoneItem key={name} zone={zone}>
           {name}
-        </div>
+        </ZoneItem>
       ))}
-  </div>
+  </Container>
 );
+
+const Container = styled.div`
+  position: absolute;
+  bottom: 50px;
+  left: 2px;
+  color: gray;
+`;
+
+const ZoneItem = styled.div<{ zone: { max: number } }>`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  height: ${({ zone }) => zone.max * 250}px;
+
+  /* Zone 6 has no upper bound */
+  &:not(:first-child) {
+    border-top: 1px solid gray;
+  }
+`;
 
 export default ZoneAxis;
