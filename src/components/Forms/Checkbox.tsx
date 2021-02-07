2c56acd0939a603cd3afef5f7a8f2db5256d5022
Rene Saarsoo <nene@triin.net>
2021-02-07 20:02:03 +0200

    Use styled-components in Checkbox

--- src/components/Forms/Checkbox.tsx
+++ src/components/Forms/Checkbox.tsx
@@ -1,14 +1,14 @@
 import React from "react";
+import styled from "styled-components";
 import { Label } from "../Label/Label";
-import "./Checkbox.css";
 
 const Checkbox = (props: {
   label: string;
   isSelected: boolean;
   onCheckboxChange: Function;
 }) => (
-  <div className="form-check">
-    <Label>
+  <Container>
+    <CheckboxLabel>
       <input
         type="checkbox"
         name={props.label}
@@ -17,8 +17,18 @@ const Checkbox = (props: {
         className="form-check-input"
       />
       {props.label}
-    </Label>
-  </div>
+    </CheckboxLabel>
+  </Container>
 );
+
+const Container = styled.div`
+  display: inline-block;
+  padding: 10px 10px 10px 0;
+`;
+
+const CheckboxLabel = styled(Label)`
+  font-size: 16px;
+  color: gray;
+`;
 
 export default Checkbox;
