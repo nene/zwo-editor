9718f74cf9babb7206dbad60c084783fb54f0bee
Rene Saarsoo <nene@triin.net>
2021-02-07 21:16:47 +0200

    Proposal for alternative Distance/Duration handling

--- src/types/Length.ts
+++ src/types/Length.ts
@@ -7,3 +7,19 @@ export class Distance {
 }
 
 export type Length = Distance | Duration;
+
+// Alternative approach
+//
+// This way we could avoid storing class instances to Redux,
+// which eliminates problems with serialization & deserialization.
+/*
+export type Distance = { meters: number };
+export type Duration = { seconds: number };
+export type Length = Distance | Duration;
+
+export const Distance = (meters: number) => ({ meters });
+export const Duration = (seconds: number) => ({ seconds });
+
+export const isDistance = (obj: Length): obj is Distance => 'meters' in obj;
+export const isDuration = (obj: Length): obj is Duration => 'seconds' in obj;
+*/
