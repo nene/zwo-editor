91dc6fcff7831035171d2e4843fecc8e017c7b68
Rene Saarsoo <nene@triin.net>
2021-02-07 19:59:40 +0200

    Use styled-components in Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,24 +1,44 @@
 import React from "react";
+import styled from "styled-components";
 import { GITHUB_URL } from "../../const";
-import "./Footer.css";
 
 export default function Footer() {
   return (
-    <div className="footer">
-      <ul>
-        <li>
+    <Container>
+      <List>
+        <Item>
           Zwift Workout v1.7 (
           <a href={`${GITHUB_URL}/blob/master/LICENSE.md`} target="blank">
             open source / MIT license
           </a>
           )
-        </li>
-        <li>
+        </Item>
+        <Item>
           <a href={`${GITHUB_URL}/issues`} target="blank">
             Report an issue
           </a>
-        </li>
-      </ul>
-    </div>
+        </Item>
+      </List>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  padding: 10px;
+  font-size: 12px;
+  text-align: center;
+  background-color: black;
+  color: white;
+`;
+
+const List = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
+
+const Item = styled.li`
+  display: inline;
+  padding: 0 10px;
+  margin: 0;
+`;
