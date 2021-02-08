ed2d4abc317b31b6425cb2f0d4a5e4d344e1a4eb
Rene Saarsoo <nene@triin.net>
2021-02-08 21:44:35 +0200

    Hint the editability of title with small icon

--- src/components/EditorPage/Title.tsx
+++ src/components/EditorPage/Title.tsx
@@ -1,3 +1,5 @@
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import styled from "styled-components";
 
@@ -18,7 +20,9 @@ const Title: React.FC<TitleProps> = ({
 }) => {
   return (
     <Container onClick={onClick}>
-      <Heading>{name}</Heading>
+      <Heading>
+        {name} <StyledIcon icon={faEdit} />
+      </Heading>
       <div>{description}</div>
       <Author>{author ? `by ${author}` : ""}</Author>
     </Container>
@@ -33,6 +37,12 @@ const Container = styled.div`
 const Heading = styled.h1`
   padding: 10px 0;
   margin: 0;
+`;
+
+const StyledIcon = styled(FontAwesomeIcon)`
+  margin-left: 10px;
+  font-size: 21px;
+  color: gray;
 `;
 
 const Author = styled.p`
