8d2f709f6de9442eb3d60c851cfbfc2dfc72e1d5
Rene Saarsoo <nene@triin.net>
2021-02-08 18:50:42 +0200

    Remove social media tags

--- src/components/Head/Head.tsx
+++ src/components/Head/Head.tsx
@@ -14,15 +14,6 @@ const Head = ({ name, description }: HeadProps) => (
         : "My Workout - Zwift Workout Editor"}
     </title>
     <meta name="description" content={description} />
-    <meta
-      property="og:title"
-      content={
-        name
-          ? `${name} - Zwift Workout Editor`
-          : "My Workout - Zwift Workout Editor"
-      }
-    />
-    <meta property="og:description" content={description} />
   </Helmet>
 );
 
