89165c37f61e7bb62ee4825a27f20e7848605167
Rene Saarsoo <nene@triin.net>
2021-02-08 16:11:21 +0200

    Throw away Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./Home";
 import EditorPage from "./components/EditorPage/EditorPage";
 
 export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/editor/:id" component={EditorPage} />
         <Route path="/">
-          <Home />
+          <EditorPage />
         </Route>
       </Switch>
     </Router>
--- src/const.ts
+++ src/const.ts
@@ -1,2 +1,1 @@
-export const PUBLIC_URL = "https://www.zwiftworkout.com/";
 export const GITHUB_URL = "https://github.com/breiko83/zwo-editor";
