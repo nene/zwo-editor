34fef774eb902332ee3fdfce83ff2c29cf248146
Rene Saarsoo <nene@triin.net>
2021-02-08 18:40:27 +0200

    Updates

---
+++ precache-manifest.68c2d8bf6eff9f0d00e883379ffe0f79.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cbdd29895a5d0a2bba870f5cd2cfb7d",
+    "url": "/zwo-editor/index.html"
+  },
+  {
+    "revision": "0be93a62d6db62ad8dd1",
+    "url": "/zwo-editor/static/css/2.6780e105.chunk.css"
+  },
+  {
+    "revision": "c1c20c13ce21ca7583eb",
+    "url": "/zwo-editor/static/css/main.c95c7188.chunk.css"
+  },
+  {
+    "revision": "0be93a62d6db62ad8dd1",
+    "url": "/zwo-editor/static/js/2.5cd096c7.chunk.js"
+  },
+  {
+    "revision": "2979a3022cd3d9958d12e9050e83b940",
+    "url": "/zwo-editor/static/js/2.5cd096c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1c20c13ce21ca7583eb",
+    "url": "/zwo-editor/static/js/main.0de1b32a.chunk.js"
+  },
+  {
+    "revision": "a84b38392036d251a67d",
+    "url": "/zwo-editor/static/js/runtime-main.6d61b5cd.js"
+  },
+  {
+    "revision": "d3e319d672e63ee77d41fd9d001609ac",
+    "url": "/zwo-editor/static/media/freeride.d3e319d6.svg"
+  },
+  {
+    "revision": "178113c8bb5c39647cad744faa021288",
+    "url": "/zwo-editor/static/media/interval.178113c8.svg"
+  },
+  {
+    "revision": "7e9a2283a61eec5097a62b35e5fec0c6",
+    "url": "/zwo-editor/static/media/steady.7e9a2283.svg"
+  },
+  {
+    "revision": "a32c38a203a966cc82397660187e9f88",
+    "url": "/zwo-editor/static/media/warmdown.a32c38a2.svg"
+  },
+  {
+    "revision": "edbffcc745d194ea27157d9826264c89",
+    "url": "/zwo-editor/static/media/warmup.edbffcc7.svg"
+  }
+]);
