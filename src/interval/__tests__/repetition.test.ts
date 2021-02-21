b3f6cfebefa2e26e24b87df788ac9310feab958b
Rene Saarsoo <nene@triin.net>
2021-02-21 21:50:13 +0000

    Split repetition-instructions between bars

--- src/interval/__tests__/repetition.test.ts
+++ src/interval/__tests__/repetition.test.ts
@@ -2,6 +2,7 @@ import { repetitionToSteadyIntervals } from "../repetition";
 import { Duration } from "../../types/Length";
 import intervalFactory from "../intervalFactory";
 import BikeMode from "../../modes/BikeMode";
+import { createInstruction } from "../../types/Instruction";
 
 jest.mock("uuid", () => ({
   v4: () => `mock-id`,
@@ -24,6 +25,65 @@ describe("repetitionToSteadyIntervals()", () => {
             offIntensity: 0.75,
             onCadence: 90,
             offCadence: undefined,
+          },
+          mode
+        ),
+        mode
+      )
+    ).toMatchSnapshot();
+  });
+
+  it("distributes instructions between SteadyIntervals", () => {
+    const mode = defaultBikeMode();
+    expect(
+      repetitionToSteadyIntervals(
+        intervalFactory.repetition(
+          {
+            id: "#1",
+            repeat: 2,
+            onLength: Duration(60),
+            offLength: Duration(30),
+            onIntensity: 0.9,
+            offIntensity: 0.75,
+            onCadence: 90,
+            offCadence: undefined,
+            instructions: [
+              createInstruction(
+                {
+                  offset: Duration(10),
+                  text: "In first interval",
+                },
+                mode
+              ),
+              createInstruction(
+                {
+                  offset: Duration(60),
+                  text: "In second interval #1",
+                },
+                mode
+              ),
+              createInstruction(
+                {
+                  offset: Duration(80),
+                  text: "In second interval #2",
+                },
+                mode
+              ),
+              createInstruction(
+                {
+                  offset: Duration(100),
+                  text: "In third interval",
+                },
+                mode
+              ),
+              createInstruction(
+                {
+                  offset: Duration(150),
+                  text: "In fourth interval",
+                },
+                mode
+              ),
+            ],
           },
           mode
         ),
--- src/interval/repetition.ts
+++ src/interval/repetition.ts
@@ -1,30 +1,55 @@
 import { range } from "ramda";
 import { WorkoutMode } from "../modes/WorkoutMode";
+import { Instruction } from "../types/Instruction";
 import { RepetitionInterval, SteadyInterval } from "../types/Interval";
+import { isDuration, Length } from "../types/Length";
 import intervalFactory from "./intervalFactory";
 
 export function repetitionToSteadyIntervals(
   interval: RepetitionInterval,
   mode: WorkoutMode
 ): SteadyInterval[] {
-  return range(0, interval.repeat).flatMap(() => [
-    intervalFactory.steady(
-      {
-        length: interval.onLength,
-        intensity: interval.onIntensity,
-        cadence: interval.onCadence,
-        pace: interval.pace,
-      },
-      mode
-    ),
-    intervalFactory.steady(
-      {
-        length: interval.offLength,
-        intensity: interval.offIntensity,
-        cadence: interval.offCadence,
-        pace: interval.pace,
-      },
-      mode
-    ),
-  ]);
+  return range(0, interval.repeat).flatMap((n) => {
+    const onSize: number = writeLength(interval.onLength);
+    const offSize: number = writeLength(interval.offLength);
+    const precedingSize: number = (onSize + offSize) * n;
+
+    return [
+      intervalFactory.steady(
+        {
+          length: interval.onLength,
+          intensity: interval.onIntensity,
+          cadence: interval.onCadence,
+          pace: interval.pace,
+          instructions: interval.instructions.filter(
+            withinRange(precedingSize, precedingSize + onSize)
+          ),
+        },
+        mode
+      ),
+      intervalFactory.steady(
+        {
+          length: interval.offLength,
+          intensity: interval.offIntensity,
+          cadence: interval.offCadence,
+          pace: interval.pace,
+          instructions: interval.instructions.filter(
+            withinRange(
+              precedingSize + onSize,
+              precedingSize + onSize + offSize
+            )
+          ),
+        },
+        mode
+      ),
+    ];
+  });
+}
+
+const writeLength = (len: Length): number =>
+  isDuration(len) ? len.seconds : len.meters;
+
+function withinRange(from: number, to: number): (ins: Instruction) => boolean {
+  return (ins: Instruction) =>
+    writeLength(ins.offset) >= from && writeLength(ins.offset) < to;
 }
